6932937aec80b122898d4e7ccf3d132d66ee88ee
Jimmie Lovell <jimmieomlovell@gmail.com>
2022-04-28 16:25:34 +0300

    build(iOS): Splash/LaunchScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import {useEffect} from 'react';
 import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {NavigationContainer} from '@react-navigation/native';
-import SplashScreen from 'react-native-splash-screen';
 import ErrorBoundary from 'hoc/error-boundary';
+import {Frame} from 'components/layout';
 import wrapper from 'hoc/wrapper';
-import {DarkTheme, DefaultTheme} from 'theme';
-import Navigator from 'navigators';
 
 const App = wrapper(() => {
   const isDarkMode = useColorScheme() === 'dark';
-
-  useEffect(() => {
-    SplashScreen.hide();
-  }, []);
 
   return (
     <GestureHandlerRootView>
@@ -23,9 +15,7 @@ const App = wrapper(() => {
           barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         />
         <ErrorBoundary>
-          <NavigationContainer theme={isDarkMode ? DarkTheme : DefaultTheme}>
-            <Navigator />
-          </NavigationContainer>
+          <Frame />
         </ErrorBoundary>
       </SafeAreaView>
     </GestureHandlerRootView>
